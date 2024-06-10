97c2c51ba681389ce5578075482fa04bbcdbc0ab
JACC <93591202+JACCCostring@users.noreply.github.com>
2024-06-10 14:27:20 +0200

    Bug Fixed: road objects table info filtered

--- nvdb_beta_dialog.py
+++ nvdb_beta_dialog.py
@@ -33,17 +33,21 @@ from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
 from qgis.utils import iface
 from qgis.core import *
+# from PyQt5 import QtCore
 
 from PyQt5.QtWidgets import QCompleter, QVBoxLayout, QLabel, QTableWidgetItem, QAbstractItemView
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtCore import  QSortFilterProxyModel, pyqtSignal, QAbstractTableModel
 
-from .nvdbskriv_beta import Ui_SkrivDialog
+# from .nvdbskriv_beta import Ui_SkrivDialog
+# from .more_window import Ui_MoreDialog
+from .source_skriv_window import SourceSkrivDialog
+from .source_more_window import SourceMoreWindow
 
 from .nvdbLesWrapper import AreaGeoDataParser
 #========================================
 #includes need it for development
-import os
+# import os
 import json
 import requests
 import threading
@@ -72,9 +76,7 @@ import threading
  ***************************************************************************/
 """
 
-from PyQt5 import QtCore
-
-import os
+# import os
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -219,6 +221,8 @@ class NvdbBetaProductionDialog(QtWidgets.QDialog, FORM_CLASS):
         
         #when QSlider value change then change label_limiter_info
         self.limit_roadObject_info_inTable.valueChanged.connect(lambda: self.label_limiter_info.setText(str(self.limit_roadObject_info_inTable.value())))
+        
+        self.more_btn.clicked.connect(self.open_more_window)
 
     
 #        rest of methods===============================
@@ -335,7 +339,7 @@ class NvdbBetaProductionDialog(QtWidgets.QDialog, FORM_CLASS):
         self.visKartCheck.setChecked(False)
         
 #        clearing layer map in QGIS in case exist 
-        self.removeActiveLayers()
+        # self.removeActiveLayers()
             
         if self.nvdbIdField.text() != '':
             nvdbId = self.listOfnvdbObjects[self.nvdbIdField.text()]
@@ -456,6 +460,7 @@ class NvdbBetaProductionDialog(QtWidgets.QDialog, FORM_CLASS):
             data_size_for_info_inTable = data_size
         
         elif data_size > 1000:
+            # data_size_for_info_inTable = self.limit_roadObject_info_inTable.value()
             data_size_for_info_inTable = 1000
         
         # self.limit_roadObject_info_inTable.setValue(self.limit_roadObject_info_inTable.value())
@@ -870,19 +875,34 @@ class NvdbBetaProductionDialog(QtWidgets.QDialog, FORM_CLASS):
     def openSkrivWindow(self):        
 #        only make instance of windows if this is None
         if self.skrivWindowInstance == None:
-            self.skrivWindowInstance = QtWidgets.QDialog()
-            self.ui = Ui_SkrivDialog(self.data, self.listOfEgenskaper) #instance self.v only exist after seacrh btn is pressed
-            self.ui.setupUi(self.skrivWindowInstance)
+            # self.skrivWindowInstance = QtWidgets.QDialog()
+            
+            # self.ui = SourceSkrivDialog(self.data, self.listOfEgenskaper) #instance self.v only exist after seacrh btn is pressed
+            # self.ui.setupUi(self.skrivWindowInstance)
+            
+            self.skrivWindowInstance = SourceSkrivDialog(self.data, self.listOfEgenskaper)
+            
             self.skrivWindowInstance.show()
+            
             self.skrivWindowOpened = True
             
 #        only shows windows again if this is allready opened
         if self.skrivWindowOpened and self.skrivWindowInstance:
             self.skrivWindowInstance.show()
-            
+    
+    
+    def open_more_window(self):
+        # self.more_window = QtWidgets.QWidget(None)
+        
+        self.source_more_window = SourceMoreWindow()
+        # self.source_more_window.setupUi(self.more_window)
+        
+        # self.more_window.show()
+        self.source_more_window.show()
+        
     def onAnyFeatureSelected(self):
         self.openSkrivWindowBtn.setEnabled(True)
-        
+
     def on_objectSizeOnLayerChange(self, value):
         layer = iface.activeLayer()
     
